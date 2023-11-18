19340e2db7ea0aa02f7a7ba23dcd8ba6f2b1cb4b
srutikarlakshmi <srutikarlakshmi@gmail.com>
2023-11-18 10:46:28 +0530

    Set up the base of the weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=bangalore&appid=026127dbef7d29041239c96211e6bd04
+  // `
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Bangalore</p>
+          </div>
+          <div className="temp">
+            <h1>25°C </h1>
+          </div>
+          <div className="description">
+            <p>Cloudy with a chance of rain.</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>Feels like 30°C</p>
+          </div>
+          <div className="humidity">
+            <p>Humidity: 68%</p>
+          </div>
+          <div className="wind">12 MPH</div>
+        </div>
+      </div>
     </div>
   );
 }
