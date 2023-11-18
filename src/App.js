60151e91912c41499da9202bb1ee3073b9a9e9f3
srutikarlakshmi <srutikarlakshmi@gmail.com>
2023-11-18 11:38:15 +0530

    creating search box and connecting with API.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=026127dbef7d29041239c96211e6bd04
-  // `
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=026127dbef7d29041239c96211e6bd04`
 
-  
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
 
   return (
     <div className="App">
+      <div className="search">
+      <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
