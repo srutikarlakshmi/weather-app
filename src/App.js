fdab87177d0e3d521b142e00f7311eb67e58cd4c
srutikarlakshmi <srutikarlakshmi@gmail.com>
2023-11-18 12:14:53 +0530

    Finishing touches and project completed!!!

--- src/App.js
+++ src/App.js
@@ -2,59 +2,67 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
-  
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=026127dbef7d29041239c96211e6bd04`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=026127dbef7d29041239c96211e6bd04`;
 
   const searchLocation = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-      })
-      setLocation('')
+        setData(response.data);
+        console.log(response.data);
+      }).catch((error) => {
+        console.error('Error fetching weather data:'+ error);
+        setLocation(''); 
+      });
     }
-  }
-
+  };
 
   return (
-    <div className="App">
+    <div className="app">
       <div className="search">
-      <input
+        <input
           value={location}
-          onChange={event => setLocation(event.target.value)}
+          onChange={(event) => setLocation(event.target.value)}
           onKeyPress={searchLocation}
-          placeholder='Enter Location'
-          type="text" />
+          placeholder="Enter Location"
+          type="text"
+        />
       </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Bangalore</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>25°C </h1>
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
-            <p>Cloudy</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-        <div className="bottom">
-          <div className="feels">
-            <p className="bold">30°C</p>
-            <p>Feels Like</p>
+
+        {data.name != undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°C</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className="humidity">
-            <p className="bold">68%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className="bold">12 MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
